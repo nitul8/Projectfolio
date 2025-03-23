a3cf9c5df06f5b69c915a144ed3902fad81996de
Nitul Das <ndas6732@gmail.com>
2025-03-23 19:29:19 +0530

    refactor: update project assets and configuration; remove unused SVGs, add new image, and adjust Next.js settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,119 @@
+"use client";
+import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import {Typewriter} from "react-simple-typewriter";
+
+import nitul from "@/assets/nitul.jpeg";
+
+import {FaGithub, FaLinkedin, FaCloudSun, FaUserGraduate} from "react-icons/fa";
+import {SiLeetcode, SiGmail} from "react-icons/si";
+import {TbTicTac, TbMathSymbols, TbListDetails} from "react-icons/tb";
+
+const ProjectList = [
+    {
+        id: 1,
+        title: "To Do List",
+        icon: <TbListDetails />,
+        link: "/todolist",
+    },
+    {
+        id: 2,
+        title: "Tic Tac Toe",
+        icon: <TbTicTac />,
+        link: "/tictactoe",
+    },
+    {
+        id: 3,
+        title: "Calculator",
+        icon: <TbMathSymbols />,
+        link: "/calculator",
+    },
+    {
+        id: 4,
+        title: "Weather App",
+        icon: <FaCloudSun />,
+        link: "/weatherapp",
+    },
+];
+
+interface ProjectProps {
+    title: string;
+    icon: React.ReactNode;
+}
+const Tooltip: React.FC<ProjectProps> = (props) => {
+    return (
+        <div className="relative inline-block group">
+            <button className="relative px-6 py-3 text-sm font-semibold text-white bg-black rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-900 transition-all duration-300 overflow-hidden">
+                <span className="relative flex items-center gap-2">
+                    {props.icon}
+                    {props.title}
+                </span>
+            </button>
+        </div>
+    );
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                <Image
+                    src={nitul}
+                    alt="Profile Picture"
+                    className="rounded-full h-96 w-96 border-4 border-gray-500"
+                />
+                <div className="font-bold text-4xl mt-10">
+                    <Typewriter
+                        words={[
+                            "Hi, I'm Nitul Das",
+                            "Welcome to My Projectfolio",
+                        ]}
+                        loop={true}
+                        cursor
+                        cursorStyle="_"
+                        typeSpeed={70}
+                        deleteSpeed={50}
+                        delaySpeed={1000}
+                    />
+                </div>
+                <p className="text-xl mt-5">
+                    Best way to learn is by doing. So, I build projects.
+                </p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <ul className="flex flex-row space-x-8 mt-10">
+                    <Link href="https://github.com/nitul8" target="_blank">
+                        <FaGithub className="text-2xl hover:scale-110 transition-transform duration-300" />
+                    </Link>
+                    <Link href="https://leetcode.com/ndas6732" target="_blank">
+                        <SiLeetcode className="text-orange-400 text-2xl hover:scale-110 transition-transform duration-300" />
+                    </Link>
+                    <Link href="mailto:ndas6732@gmail.com" target="_blank">
+                        <SiGmail className="text-red-500 text-2xl hover:scale-110 transition-transform duration-300" />
+                    </Link>
+                    <Link
+                        href="https://www.linkedin.com/in/nituldas/"
+                        target="_blank"
+                    >
+                        <FaLinkedin className="text-blue-600 text-2xl hover:scale-110 transition-transform duration-300" />
+                    </Link>
+                    <Link href="https://nituldas.vercel.app/" target="_blank">
+                        <FaUserGraduate className="text-green-500 text-2xl hover:scale-110 transition-transform duration-300" />
+                    </Link>
+                </ul>
+            </div>
+            <div className="w-4/5 flex justify-center">
+                <div className="grid grid-cols-2 gap-4">
+                    {ProjectList.map((project) => (
+                        <Link href={project.link} key={project.id}>
+                            <Tooltip
+                                title={project.title}
+                                icon={project.icon}
+                            />
+                        </Link>
+                    ))}
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
---
+++ app/weatherapp/page.tsx
@@ -0,0 +1,189 @@
+"use client";
+import React, {useState} from "react";
+
+import {IoIosMoon, IoIosSunny} from "react-icons/io";
+import {LuDroplets} from "react-icons/lu";
+import {FaWind} from "react-icons/fa";
+
+const Weather = () => {
+    interface WeatherData {
+        location: {
+            name: string;
+            region: string;
+            country: string;
+            localtime: string;
+        };
+        current: {
+            temp_c: number;
+            humidity: number;
+            wind_mph: number;
+            uv: number;
+            is_day: boolean;
+            condition: {
+                icon: string;
+            };
+        };
+        forecast: {
+            forecastday: {
+                date: string;
+                day: {maxtemp_c: number; mintemp_c: number};
+            }[];
+        };
+    }
+
+    const [weather, setWeather] = useState<WeatherData | null>(null);
+    const [place, setPlace] = useState("");
+
+    const fetchWeather = async (name: string) => {
+        try {
+            const response = await fetch(
+                `http://api.weatherapi.com/v1/forecast.json?key=6e16a80e48a54f4bbe2192532252203&q=${name}&days=3&aqi=yes&alerts=yes`
+            );
+            const data = await response.json();
+            console.log(data);
+            setWeather(data);
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+        }
+    };
+
+    const formatDate = (localdate: string) => {
+        const date = new Date(localdate);
+        return new Intl.DateTimeFormat("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+        }).format(date);
+    };
+
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-sky-500 to-indigo-500">
+            <form className="mb-6 flex justify-between items-center space-x-3">
+                <input
+                    type="text"
+                    placeholder="Search for places..."
+                    className="w-4/5 max-w-lg bg-white/10 backdrop-blur-md p-3 rounded-lg shadow-lg text-white"
+                    value={place.toLowerCase()}
+                    onChange={(e) => setPlace(e.target.value)}
+                />
+                <button
+                    type="submit"
+                    className="w-2/5 max-w-lg bg-white/20 backdrop-blur-md p-3 rounded-lg shadow-lg text-white"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        if (!location) return;
+                        fetchWeather(place);
+                    }}
+                >
+                    Search
+                </button>
+            </form>
+            {weather && (
+                <div className="w-4/5 max-w-lg bg-white/10 backdrop-blur-md p-6 rounded-lg shadow-lg">
+                    <div className="flex justify-between items-center text-white">
+                        <div>
+                            <h3 className="text-xl font-semibold">
+                                {weather.location.name}
+                            </h3>
+                            <p className="text-sm text-white/80">
+                                {weather.location.region},{" "}
+                                {weather.location.country}
+                            </p>
+                        </div>
+                        <span className="text-sm flex flex-col items-end">
+                            <p className="text-sm text-white/80">
+                                {weather.location.localtime.split(" ")[1]}
+                            </p>
+                            <p className="text-sm text-white/80">
+                                {weather.location.localtime.split(" ")[0]}
+                            </p>
+                        </span>
+                    </div>
+
+                    <div className="mt-6 flex items-center justify-between">
+                        <div className="flex items-start">
+                            <span className="text-6xl font-bold text-white">
+                                {weather.current.temp_c}
+                            </span>
+                            <span className="mt-1 text-2xl text-white/80">
+                                °C
+                            </span>
+                        </div>
+                        <img
+                            src={weather.current.condition.icon}
+                            alt="Weather Icon"
+                            className="h-20 w-20"
+                        />
+                    </div>
+
+                    <div className="mt-6 grid grid-cols-3 gap-4">
+                        {[
+                            {
+                                label: "Humidity",
+                                value: `${weather.current.humidity}%`,
+                                icon: <LuDroplets />,
+                            },
+                            {
+                                label: "Wind",
+                                value: `${weather.current.wind_mph} mph`,
+                                icon: <FaWind />,
+                            },
+                            {
+                                label: "UV Index",
+                                value: `${weather.current.uv}`,
+                                icon: weather.current.is_day ? (
+                                    <IoIosSunny />
+                                ) : (
+                                    <IoIosMoon />
+                                ),
+                            },
+                        ].map((item, index) => (
+                            <div
+                                key={index}
+                                className="flex flex-col items-center text-white"
+                            >
+                                <span className="text-xl m-2">{item.icon}</span>
+                                <span className="text-sm">{item.label}</span>
+                                <span className="text-lg font-semibold">
+                                    {item.value}
+                                </span>
+                            </div>
+                        ))}
+                    </div>
+
+                    <div className="mt-6">
+                        <h4 className="text-sm font-medium text-white/80 mb-3">
+                            3-Day Forecast
+                        </h4>
+                        {[
+                            {
+                                day: "Today",
+                                temp: `${weather.forecast.forecastday[0].day.mintemp_c}° / ${weather.forecast.forecastday[0].day.maxtemp_c}°`,
+                            },
+                            {
+                                day: "Tomorrow",
+                                temp: `${weather.forecast.forecastday[1].day.mintemp_c}° / ${weather.forecast.forecastday[1].day.maxtemp_c}°`,
+                            },
+                            {
+                                day: `${formatDate(
+                                    weather.location.localtime.split(" ")[0]
+                                )}`,
+                                temp: `${weather.forecast.forecastday[2].day.mintemp_c}° / ${weather.forecast.forecastday[2].day.maxtemp_c}°`,
+                            },
+                        ].map((item, index) => (
+                            <div
+                                key={index}
+                                className="flex justify-between items-center bg-white/10 p-3 rounded-md mb-2"
+                            >
+                                <span className="text-white">{item.day}</span>
+                                <span className="text-white">{item.temp}</span>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Weather;
