2d8e7947d715d3953a5d1593f000f97b5c5736e7
Nitul Das <ndas6732@gmail.com>
2025-03-24 22:51:57 +0530

    feat: add Todo and Calculator components with basic functionality

---
+++ app/calculator/calculator.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+
+interface ButtonProps {
+    children: React.ReactNode;
+}
+
+export const Calculator = () => {
+    const [value, setValue] = React.useState<string>("");
+
+    const Button: React.FC<ButtonProps> = ({children}) => {
+        return (
+            <button
+                className="cursor-pointer h-[60px] w-[60px] border text-xl rounded-full bg-green-50 shadow-xl"
+                onClick={() => setValue(value + children)}
+            >
+                {children}
+            </button>
+        );
+    };
+    const evaluate = (e: React.FormEvent) => {
+        e.preventDefault();
+        setValue(eval(value));
+    };
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="w-[400px] border rounded-2xl bg-gray-50 shadow-xl p-[45px] scale-[0.6]">
+                <div className="input h-[100px] w-full rounded-2xl bg-green-50 border p-[0px,30px]">
+                    <input
+                        type="text"
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        className="text-3xl overflow-hidden flex items-center w-full h-full border-none outline-none p-3"
+                    />
+                </div>
+                <div style={{paddingTop: 40}}>
+                    <div className="pt-[30px] flex justify-between">
+                        <Button>1</Button>
+                        <Button>2</Button>
+                        <Button>3</Button>
+                        <Button>+</Button>
+                    </div>
+                    <div className="pt-[30px] flex justify-between">
+                        <Button>4</Button>
+                        <Button>5</Button>
+                        <Button>6</Button>
+                        <Button>-</Button>
+                    </div>
+                    <div className="pt-[30px] flex justify-between">
+                        <Button>7</Button>
+                        <Button>8</Button>
+                        <Button>9</Button>
+                        <Button>*</Button>
+                    </div>
+                    <div className="pt-[30px] flex justify-between">
+                        <Button>.</Button>
+                        <Button>0</Button>
+                        <Button>/</Button>
+                        <button
+                            className="cursor-pointer h-[60px] w-[60px] border text-xl rounded-full bg-green-50 shadow-xl"
+                            onClick={evaluate}
+                        >
+                            =
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
--- app/todolist/page.tsx
+++ app/todolist/page.tsx
@@ -1,76 +1,10 @@
-"use client";
-import React, {useState} from "react";
-import {IoCheckmarkDoneCircleOutline} from "react-icons/io5";
+import Todolist from "./todolist";
 
-interface TodoProps {
-    id: number;
-    text: string;
-}
-
-const TodoPage = () => {
-    const [TodoList, setTodoList] = useState<TodoProps[]>([]);
-    const [input, setInput] = useState<string>("");
-
-    const addTodo = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (!input) return;
-        setTodoList([
-            ...TodoList,
-            {
-                id: TodoList.length + 1,
-                text: input,
-            },
-        ]);
-        setInput("");
-    };
-
-    const deleteTodo = (id: number) => {
-        setTodoList([...TodoList.filter((todo) => todo.id !== id)]);
-    };
-
-    const Todo: React.FC<TodoProps> = (props) => {
-        return (
-            <li className="flex gap-2 items-center justify-center">
-                <button
-                    className="bg-red-500 text-white font-bold rounded-full"
-                    onClick={() => deleteTodo(props.id)}
-                >
-                    <IoCheckmarkDoneCircleOutline className="text-2xl" />
-                </button>
-                <span className="m-3">{props.text}</span>
-            </li>
-        );
-    };
-
-    return (
-        <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
-            <form action="" className="flex gap-2 justify-center">
-                <input
-                    type="text"
-                    className="border p-2"
-                    name="text"
-                    onChange={(e) => {
-                        setInput(e.target.value);
-                    }}
-                    value={input}
-                    placeholder="Enter Your Task . . ."
-                />
-                <button
-                    type="submit"
-                    className="bg-green-500 p-2"
-                    onClick={addTodo}
-                >
-                    Add
-                </button>
-            </form>
-
-            <div className="flex flex-col gap-2">
-                {TodoList.map((todo) => (
-                    <Todo key={todo.id} id={todo.id} text={todo.text} />
-                ))}
-            </div>
-        </div>
-    );
+export const metadata = {
+    title: "To Do List",
 };
 
+const TodoPage = () => {
+    return <Todolist />;
+};
 export default TodoPage;
